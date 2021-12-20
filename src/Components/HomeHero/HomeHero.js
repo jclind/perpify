90ae2d64a8892ae3b0b78df31af92cbafd2b4ac4
Jesse Lind <Jesseclind@gmail.com>
2021-12-20 16:24:51 -0500

    Work on hero component / add search bar in hero

--- src/Components/HomeHero/HomeHero.js
+++ src/Components/HomeHero/HomeHero.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeHero.scss'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 const HomeHero = () => {
+  const [searchRecipeVal, setSearchRecipeVal] = useState('')
+
   return (
     <div className='home-hero'>
       <img src='/images/home-images/hero.jpg' alt='Assorted Foods Background' />
       <div className='hero-overlay'>
         <h3 className='text'>Save money. Reduce stress. Be healthy.</h3>
-        <button className='btn action-btn'>Recipes</button>
+        <label
+          htmlFor='.search-recipes-input'
+          className='search-recipes-input-label'
+        >
+          <AiOutlineSearch className='icon' />
+          <input
+            className='search-recipes-input'
+            placeholder='Search All Recipes'
+            onChange={e => setSearchRecipeVal(e.target.value)}
+            value={searchRecipeVal}
+          />
+        </label>
       </div>
     </div>
   )
