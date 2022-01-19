d4856db795a848e7e20188bec7e2a802ef8eeb24
Jesse Lind <Jesseclind@gmail.com>
2022-01-19 16:49:45 -0500

    feat: add recipe image and header data to SingleRecipe component

--- src/Components/SingleRecipe/SingleRecipe.js
+++ src/Components/SingleRecipe/SingleRecipe.js
@@ -4,6 +4,12 @@ import { useParams } from 'react-router-dom'
 import { useRecipe } from '../../context/RecipeContext'
 import { TailSpin } from 'react-loader-spinner'
 
+import {
+  AiOutlineClockCircle,
+  AiOutlineUsergroupAdd,
+  AiOutlineStar,
+} from 'react-icons/ai'
+
 const SingleRecipe = () => {
   const [currRecipe, setCurrRecipe] = useState(null)
   const [loading, setLoading] = useState(true)
@@ -35,7 +41,41 @@ const SingleRecipe = () => {
           />
         </div>
       ) : (
-        <p>single Recipe {currRecipe && currRecipe.title}</p>
+        <div className='recipe-container'>
+          <div className='header-content'>
+            <div className='recipe-image-container'>
+              <img src={currRecipe.url} alt={currRecipe.title} />
+            </div>
+            <div className='description-content'>
+              <h1 className='title'>{currRecipe.title}</h1>
+              <p className='description'>
+                Put down the fork and knife… here's a Chicago deep-dish pizza
+                that you can actually eat by holding it in your hands! A
+                beautiful golden crust contains a meaty, cheesy, saucy filling
+                with Italian sausage, sweet peppers, and 3 types of cheese. The
+                sauce and cheese char slightly on the outside as they bake,
+                resulting in a super savory taste.
+              </p>
+              <div className='recipe-data'>
+                <div className='time data-element'>
+                  <AiOutlineClockCircle className='icon' />
+                  <h3>Total Time</h3>
+                  <div className='data'>{currRecipe.totalTime} min.</div>
+                </div>
+                <div className='servings data-element'>
+                  <AiOutlineUsergroupAdd className='icon' />
+                  <h3>Servings</h3>
+                  <div className='data'>{currRecipe.servings}</div>
+                </div>
+                <div className='rating data-element'>
+                  <AiOutlineStar className='icon' />
+                  <h3>Rating</h3>
+                  <div className='data'>{currRecipe.rating}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   )
