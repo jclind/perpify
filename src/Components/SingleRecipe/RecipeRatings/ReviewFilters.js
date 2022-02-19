e935e5691e2fc4f0b71a36115ac8d849be23f4ff
Jesse Lind <Jesseclind@gmail.com>
2022-02-18 22:44:52 -0500

    feat: begin adding filters to reviews section

---
+++ src/Components/SingleRecipe/RecipeRatings/ReviewFilters.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Select from 'react-select'
+
+const options = [
+  { value: 'new', label: 'Date: Newest' },
+  { value: 'old', label: 'Date: Oldest' },
+  { value: 'positive', label: 'Most Positive' },
+  { value: 'negative', label: 'Most Negative' },
+]
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    background: '#eeeeee',
+    minHeight: '40px',
+    height: '40px',
+    width: '200px',
+    boxShadow: state.isFocused ? null : null,
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: 'hsl(0, 0%, 0%)',
+    fontWeight: '500',
+    paddingBottom: '3px',
+  }),
+
+  valueContainer: (provided, state) => ({
+    ...provided,
+    height: '40px',
+    padding: '0 6px',
+  }),
+
+  input: (provided, state) => ({
+    ...provided,
+    margin: '0px',
+  }),
+  indicatorSeparator: state => ({
+    display: 'none',
+  }),
+  indicatorsContainer: (provided, state) => ({
+    ...provided,
+    height: '40px',
+  }),
+}
+
+const ReviewFilters = () => {
+  const handleSelectChange = () => {}
+
+  return (
+    <div>
+      <div className='review-filters'>
+        <Select
+          options={options}
+          styles={customStyles}
+          isSearchable={false}
+          isClearable={false}
+          className='select'
+          onChange={handleSelectChange}
+          defaultValue={options[0]}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ReviewFilters
