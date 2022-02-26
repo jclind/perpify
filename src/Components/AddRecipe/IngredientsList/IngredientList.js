97229caad5cc510788af0f72a698fc3f4ea0b367
Jesse Lind <Jesseclind@gmail.com>
2022-02-25 20:41:39 -0500

    fix: add responsiveness to addRecipe page

--- src/Components/AddRecipe/IngredientsList/IngredientList.js
+++ src/Components/AddRecipe/IngredientsList/IngredientList.js
@@ -58,7 +58,8 @@ const IngredientList = ({
       borderColor: '#9e9e9e',
       minHeight: '47px',
       height: '47px',
-      width: '160px',
+      width: '100%',
+      minWidth: '160px',
       boxShadow: state.isFocused ? null : null,
     }),
     singleValue: (provided, state) => ({
@@ -207,7 +208,7 @@ const IngredientList = ({
       )}
       <div className='inputs'>
         <input
-          type='text'
+          type='number'
           className='ingredient-quantity'
           value={quantity}
           onChange={e => {
