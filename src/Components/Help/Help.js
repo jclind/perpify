066669890ae3fdd56fcf6b562f3174b8a0822382
Jesse Lind <Jesseclind@gmail.com>
2022-02-28 21:23:34 -0500

    feat: connect help form to formspree

--- src/Components/Help/Help.js
+++ src/Components/Help/Help.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
-import FormInput from '../Form/FormInput'
+import { useForm, ValidationError } from '@formspree/react'
 import './Help.scss'
 
 const options = [
@@ -17,6 +17,11 @@ const customStyles = {
     height: '40px',
     minWidth: '100%',
     boxShadow: state.isFocused ? null : null,
+    outline: 'none',
+    border: state.isFocused ? '1px solid #00adb5' : '1px solid #d6d6d6',
+    ':hover': {
+      border: '1px solid #00adb5',
+    },
   }),
   singleValue: (provided, state) => ({
     ...provided,
@@ -42,38 +47,101 @@ const customStyles = {
     ...provided,
     height: '40px',
   }),
+  placeholder: (provided, state) => ({
+    ...provided,
+    fontWeight: '500',
+    color: '#bebebe',
+  }),
 }
 
 const Help = () => {
-  const [selectOption, setSelectOption] = useState(options[0])
-  const [title, setTitle] = useState('title')
+  const [selectOption, setSelectOption] = useState(null)
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const [error, setError] = useState('')
+
+  const [formState, submitFormspree] = useForm('xknyboeq')
 
   const handleSelectChange = e => {
     setSelectOption(e)
   }
+  const clearForm = () => {
+    setSelectOption(null)
+    setTitle('')
+    setDescription('')
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!selectOption) return setError('Please Enter Category')
+
+    submitFormspree(e)
+    clearForm()
+  }
+
   return (
     <div className='help-page page'>
       <div className='form-container'>
-        <form className='report-form'>
-          <div className='select-container'>
-            <div className='text'>I am:</div>
-            <Select
-              options={options}
-              styles={customStyles}
-              isSearchable={false}
-              isClearable={false}
-              className='select'
-              onChange={handleSelectChange}
-              placeholder={'Select an option...'}
-            />
+        {formState.succeeded ? (
+          <div className='form-success'>
+            <h1>Form Submitted!</h1>
+            <div className='image-container'>
+              <img src='/images/form-submitted.svg' alt='letter submitted' />
+            </div>
+            <button
+              className='submit-another-form-btn btn'
+              onClick={() => window.location.reload()}
+            >
+              Submit Another Form
+            </button>
           </div>
-          <input
-            type='text'
-            className='title'
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-        </form>
+        ) : (
+          <form className='report-form' onSubmit={handleSubmit}>
+            <h4 className='title'>Help Form</h4>
+            {error && <div className='error'>{error}</div>}
+            <div className='select-container'>
+              <div className='text'>Category</div>
+              <Select
+                options={options}
+                styles={customStyles}
+                isSearchable={false}
+                isClearable={false}
+                className='select'
+                value={selectOption}
+                onChange={handleSelectChange}
+                placeholder={'Select an option...'}
+                name='category'
+              />
+            </div>
+            <label htmlFor='' className='title-input-label'>
+              <div className='text'>Title</div>
+              <input
+                type='text'
+                name='title'
+                className='title-input'
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                placeholder='Enter a title'
+                required={true}
+              />
+            </label>
+            <label htmlFor='' className='description-input-label'>
+              <div className='text'>Description</div>
+              <textarea
+                className='description-textarea'
+                value={description}
+                name='description'
+                onChange={e => setDescription(e.target.value)}
+                placeholder='Enter a description'
+                rows='5'
+                required={true}
+              ></textarea>
+            </label>
+            <button type='submit' className='submit-help-btn btn'>
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </div>
   )
